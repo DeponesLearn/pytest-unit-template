7cccd82fbe1807badb948cbe2a60076eb26be0d1
berkacunas <90577758+berkacunas@users.noreply.github.com>
2026-09-02 13:23:21 +0300

    add modular calculator dir in src dir

---
+++ src/calculator/core.py
@@ -0,0 +1,15 @@
+def add(n1: int, n2: int) -> int:
+
+    return n1 + n2
+
+def sub(n1: int, n2: int) -> int:
+
+    return n1 - n2
+
+def mul(n1: int, n2: int) -> int:
+
+    return n1 * n2
+
+def div(n1: int, n2: int) -> float:
+
+    return n1 / n2
